3ea9eacb25530e07880cc80fe3e59de78b94e4d1
Tyler Uttech <tuttech4@gmail.com>
2023-09-20 13:26:08 -0500

    Updates to FileInOut assignment

--- src/edu/uwp/csci/Cs242/assignment/a01/fileinout/FileInOut.java
+++ src/edu/uwp/csci/Cs242/assignment/a01/fileinout/FileInOut.java
@@ -1,4 +1,12 @@
 package edu.uwp.csci.Cs242.assignment.a01.fileinout;
 
 public class FileInOut {
+    private static final String DEFAULTINFILENAME = "default_in.txt"; // string constant that holds the name of the default input file
+    private static final String DEFAULTOUTFILENAME = "default_out.txt"; // string constant that holds the name of the default output file
+    private String inFilename = ""; // variable that holds the name of the input file
+    private String outFilename = ""; // variable that holds the name of the output file
+
+    // TODO: write a meaningful constructor.
+    //public FileInOut()
+
 }
